ae978a80adcb6a9c733693f512f907a9d84cb150
deathatred <maksym.linynskyi@gmail.com>
2026-03-12 13:47:25 +0200

    feat: add block controller
    Added block controller using DoTween MoveX

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,24 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private float _distance = 3f;
+    [SerializeField] private float _duration = 1f;
+
+    private Tween _moveTween;
+
+    private void Start()
+    {
+        MoveBlock();
+    }
+    private void MoveBlock()
+    {
+        _moveTween = _rigidBody.DOMoveX(_distance, _duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+    private void OnDisable()
+    {
+        _moveTween?.Kill();
+    }
+}
