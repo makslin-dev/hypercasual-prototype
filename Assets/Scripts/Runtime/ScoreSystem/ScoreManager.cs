8e85c3020e88a3b4301a477ff38f006449de7513
deathatred <maksym.linynskyi@gmail.com>
2026-03-18 17:03:26 +0200

    feat: update block prefab, update camera pos
    now default block is 5x5
    camera is now is in more efficient position

--- Assets/Scripts/Runtime/ScoreSystem/ScoreManager.cs
+++ Assets/Scripts/Runtime/ScoreSystem/ScoreManager.cs
@@ -6,10 +6,6 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private GameManager _gameManager;
     public int Score { get; private set; } = -1;
 
-    private void Update()
-    {
-        print(Score);
-    }
     private void Awake()
     {
         _gameManager.OnNextBlockStart += IncreaseScore;
