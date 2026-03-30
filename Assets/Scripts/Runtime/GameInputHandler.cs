3294fa5e7b560a3674d4f58c4d2ae4b131c6684c
deathatred <maksym.linynskyi@gmail.com>
2026-03-30 11:59:12 +0300

    fix: add ui click bug
    Now UI clicks doent trigger in-game actions

--- Assets/Scripts/Runtime/GameInputHandler.cs
+++ Assets/Scripts/Runtime/GameInputHandler.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using static GameController;
 
@@ -15,10 +17,27 @@ public class GameInputHandler : MonoBehaviour, IPlayerActions
     {
         if (context.started)
         {
-            print("here");
-            OnBlockPlaced.Invoke();
+            if (IsPointerOverUI())
+            {
+                return;
+            }
+
+            OnBlockPlaced?.Invoke();
         }
     }
 
-    
+    private bool IsPointerOverUI()
+    {
+        Vector2 pointerPosition = Pointer.current != null ? Pointer.current.position.ReadValue() : Vector2.zero;
+
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = pointerPosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        return results.Count > 0;
+    }
+
+
 }
